fe40074a50f87e0102625d9c6992f5b9daae58df
Gordon Watts <gwatts@uw.edu>
2020-01-30 23:29:40 -0800

    Slowly adding tests and code
    Framework to start downloading files

--- ServiceX_fe/servicex.py
+++ ServiceX_fe/servicex.py
@@ -1,7 +1,42 @@
 # Main frontend interface
 import pandas as pd
-from typing import Union, List
+from typing import Union, List, Tuple, Optional, Dict, Any, Iterable
 import aiohttp
+import asyncio
+
+
+async def _get_transform_status(client: aiohttp.ClientSession, endpoint: str,
+                                request_id: str) -> Tuple[Optional[int], int]:
+    '''
+    Internal routine that queries for the current stat of things. We expect the following things
+    to come back:
+        - files-processed
+        - files-remaining
+        - files-skipped
+        - request-id
+        - stats
+
+    Arguments:
+        endpoint            Web API address where servicex lives
+        request_id          The id of the request to check up on
+
+    Returns
+        files_remaining     How many files remain to be processed. None if the number has not yet
+                            been determined
+        file_processed      How many files have been successfully processed by the system.
+    '''
+    async with client.get(f'{endpoint}/transformation/{request_id}/status') as response:
+        info = await response.json()
+        files_remaining = None if (('files-remaining' not in info) or
+                                   (info['files-remaining'] is None)) \
+            else int(info['files-remaining'])
+        files_processed = int(info['files-processed'])
+        return files_remaining, files_processed
+
+
+async def _download_new_files(files_queued: Iterable[str], endpoint: str,
+                              request_id: str) -> Dict[str, Any]:
+    return {}
 
 
 async def get_data(selection_query: str, datasets: Union[str, List[str]],
@@ -43,7 +78,26 @@ async def get_data(selection_query: str, datasets: Union[str, List[str]],
             assert response.status == 200
             request_id = (await response.json())["request_id"]
 
-        # Load all the files as they become availible.
+        # Sit here waiting for the results to come in. In case there are missing items
+        # in the minio stream, we will avoid counting that. That should be an explicit error taken
+        # care of further on down in the code.
+        done = False
+        files_downloading = {}
+        last_files_processed = 0
+        while not done:
+            await asyncio.sleep(5.0)
+            files_remaining, files_processed = await _get_transform_status(client,
+                                                                           servicex_endpoint,
+                                                                           request_id)
+            if files_processed != last_files_processed:
+                new_downloads = await _download_new_files(files_downloading.keys(),
+                                                          servicex_endpoint, request_id)
+                files_downloading.update(new_downloads)
+                last_files_processed = files_processed
+
+            done = (files_remaining is not None) and files_remaining == 0
+
+        # Now, wait for all of them to complete so we can stich the files together.
 
         # return the result
         return pd.DataFrame()
--- tests/test_servicex.py
+++ tests/test_servicex.py
@@ -2,16 +2,47 @@ import pytest
 from requests.api import patch  # NOQA
 import ServiceX_fe as fe
 import pandas as pd
+import json
+import asyncio
+from unittest import mock
+
+
+class ClientSessionMocker:
+    def __init__(self, text, status):
+        self._text = text
+        self.status = status
+
+    async def text(self):
+        return self._text
+
+    async def json(self):
+        return json.loads(self._text)
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+    async def __aenter__(self):
+        return self
 
 
 @pytest.fixture()
-def good_transform_request(requests_mock):
-    requests_mock.post(f'http://localhost:5000/servicex/transformation',
-                       text='{"request_id": "1234"}')
+def good_transform_request(mocker):
+    r = ClientSessionMocker(json.dumps({"request_id": "1234-4433-111-34-22-444"}), 200)
+    mocker.patch('aiohttp.ClientSession.post', return_value=r)
+    return r
 
+@pytest.fixture()
+def time_is_short(mocker):
+    # TODO: Make sure this allows other co-routines to run!
+    class AsyncMock(mock.MagicMock):
+        async def __call__(self, *args, **kwargs):
+            return super(AsyncMock, self).__call__(*args, **kwargs)
+
+    mocker.patch('asyncio.sleep', new_callable=AsyncMock)
 
 @pytest.mark.asyncio
-async def test_good_run_single_ds(good_transform_request):
+async def test_good_run_single_ds(good_transform_request, time_is_short):
     'Simple run with expected results'
     r = await fe.get_data('(valid qastle string)', 'one_ds')
     assert isinstance(r, pd.DataFrame)
+    assert len(r) == 4
