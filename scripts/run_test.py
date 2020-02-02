2440fef4c7a078faf9cea1051b054e3428b3f0f3
Gordon Watts <gwatts@uw.edu>
2020-02-02 14:30:36 -0800

    Update to use non-async version

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -4,15 +4,13 @@
 # An example endpoint (pass as arg to this script): http://localhost:5000/servicex
 import sys
 import ServiceX_fe
-import asyncio
 
 
-# TODO: This should not be async, but until we implement that bit, we will leave it like that.
-async def run_query(endpoint: str) -> None:
-    r = await ServiceX_fe.get_data_async("(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')", "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00", servicex_endpoint=endpoint)
+def run_query(endpoint: str) -> None:
+    r = ServiceX_fe.get_data("(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')", "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00", servicex_endpoint=endpoint)
     print(r)
 
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2
-    asyncio.run(run_query(sys.argv[1]))
+    run_query(sys.argv[1])
