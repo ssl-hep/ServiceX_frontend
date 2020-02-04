4dfe94ad70a00c9312a28387c6ad9d2b0db45f06
Gordon Watts <gwatts@uw.edu>
2020-02-03 22:44:31 -0800

    Migrate tests to do 4 simultanious
    Will now do downloads and runs.

--- tests/test_servicex.py
+++ tests/test_servicex.py
@@ -35,25 +35,40 @@ class fget_object_good_copy_callable(mock.MagicMock):
                     args[2])
 
 
-def make_minio_file(mocker, fname):
-    r = mocker.MagicMock()
+def make_minio_file(fname):
+    r = mock.MagicMock()
     r.object_name = fname
     return r
 
 
 @pytest.fixture()
 def files_back_1(mocker):
-    mocker.patch('minio.api.Minio.list_objects', return_value=[make_minio_file(mocker, 'root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000001.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio')])
+    mocker.patch('minio.api.Minio.list_objects', return_value=[make_minio_file('root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000001.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio')])
     mocker.patch('minio.api.Minio.fget_object', new_callable=fget_object_good_copy_callable)
     return None
 
 
 @pytest.fixture()
 def files_back_2(mocker):
-    mocker.patch('minio.api.Minio.list_objects', return_value=[make_minio_file(mocker, 'root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000001.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio'),
-                                                               make_minio_file(mocker, 'root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000002.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio')])
+    mocker.patch('minio.api.Minio.list_objects', return_value=[make_minio_file('root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000001.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio'),
+                                                               make_minio_file('root:::dcache-atlas-xrootd-wan.desy.de:1094::pnfs:desy.de:atlas:dq2:atlaslocalgroupdisk:rucio:mc15_13TeV:8a:f1:DAOD_STDM3.05630052._000002.pool.root.198fbd841d0a28cb0d9dfa6340c890273-1.part.minio')])
     mocker.patch('minio.api.Minio.fget_object', new_callable=fget_object_good_copy_callable)
     return None
+
+
+class list_objects_callable(mock.MagicMock):
+    def __call__(self, *args, **kwargs):
+        assert len(args) == 1
+        req_id = args[0]
+        assert isinstance(req_id, str)
+        number = int(req_id)
+        return [make_minio_file(f'root:::dcache-atlas-{i}') for i in range(0, number)]
+
+
+@pytest.fixture()
+def four_sets_of_files_back(mocker):
+    mocker.patch('minio.api.Minio.list_objects', new_callable=list_objects_callable)
+    mocker.patch('minio.api.Minio.fget_object', new_callable=fget_object_good_copy_callable)
 
 
 @pytest.fixture()
@@ -67,6 +82,23 @@ def good_transform_request(mocker):
     mocker.patch('aiohttp.ClientSession.get', return_value=r2)
 
     return None
+
+
+@pytest.fixture()
+def four_good_transform_requests(mocker):
+    '''
+    We will setup four transform requests.
+    '''
+    # Get the original queries
+    r1 = ClientSessionMocker(json.dumps({"request_id": "1"}), 200)
+    r2 = ClientSessionMocker(json.dumps({"request_id": "2"}), 200)
+    r3 = ClientSessionMocker(json.dumps({"request_id": "3"}), 200)
+    r4 = ClientSessionMocker(json.dumps({"request_id": "4"}), 200)
+    mocker.patch('aiohttp.ClientSession.post', side_effect=[r1, r2, r3, r4])
+
+    # Now get back the returns, which are just going to always be the same.
+    g1 = ClientSessionMocker(json.dumps({"files-remaining": "0", "files-processed": "4"}), 200)
+    mocker.patch('aiohttp.ClientSession.get', return_value=g1)
 
 
 @pytest.fixture()
@@ -117,6 +149,21 @@ def test_run_with_running_event_loop(good_transform_request, time_is_short, file
         assert len(r) == 283458
     loop = asyncio.get_event_loop()
     loop.run_until_complete(doit())
+
+
+@pytest.mark.asyncio
+async def test_run_with_four_queries(four_good_transform_requests, time_is_short, four_sets_of_files_back):
+    'Try to run four transform requests at same time. Make sure each retursn what we expect.'
+    r1 = fe.get_data_async('(valid qastle string)', 'one_ds')
+    r2 = fe.get_data_async('(valid qastle string)', 'two_ds')
+    r3 = fe.get_data_async('(valid qastle string)', 'three_ds')
+    r4 = fe.get_data_async('(valid qastle string)', 'four_ds')
+    all_wait = await asyncio.gather(*[r1, r2, r3, r4])
+    assert len(all_wait) == 4
+    for cnt, r in enumerate(all_wait):
+        assert isinstance(r, pd.DataFrame)
+        assert len(r) == 283458*(cnt+1)
+
 
 # TODO:
 # Other tests
