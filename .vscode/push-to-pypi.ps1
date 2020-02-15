e4cd21da368621ab59b8d4e1cb7f25db3ff70602
Gordon Watts <gwatts@uw.edu>
2020-02-15 00:11:36 -0800

    Clean up before and after pushing
    Prevents debugging and IDE errors.

--- .vscode/push-to-pypi.ps1
+++ .vscode/push-to-pypi.ps1
@@ -1,4 +1,8 @@
-Remove-Item -ErrorAction SilentlyContinue ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
 Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
 python setup.py sdist bdist_wheel
 twine upload dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
