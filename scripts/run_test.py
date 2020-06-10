33e7fc313142c52fb87036b45c2bf9cc5e430feb
Gordon Watts <gwatts@uw.edu>
2020-06-09 23:10:25 -0700

    Fix up some flake8 errors

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -1,17 +1,17 @@
 # Tests against a ServiceX instance. This is not meant to be a complete
 # test of every code path. Just a sanity check. The unit tests are meant to
 # do that sort of testing.
-# An example endpoint (pass as arg to this script): http://localhost:5000/servicex
+# An example endpoint (pass as arg to this script):
+#       http://localhost:5000/servicex
 import sys
 from servicex import ServiceX
 
 
 def run_query(endpoint: str) -> None:
     ds = ServiceX(
-        "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00",
-        endpoint
-        )
-    r = ds.get_data_rootfiles("(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')")
+        "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00",  # NOQA
+        endpoint)
+    r = ds.get_data_rootfiles("(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')")  # NOQA
     print(r)
 
 
@@ -20,5 +20,5 @@ if __name__ == '__main__':
     run_query(sys.argv[1])
 
 # TODO: Make sure files download while other things are still running
-# TODO: The total number of files sometimes balloons up to 22 from 17 for this example.
-#       Should not happen.
+# TODO: The total number of files sometimes balloons up to 22 from 17 for this
+#       example. Should not happen.
