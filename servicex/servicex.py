f87f3cb701fb18582f5cbeb42da65b140771330f
Gordon Watts <gwatts@uw.edu>
2020-06-11 17:07:44 -0700

    In-process refactoring how we do requests
    Trying to make the logic more "straight" - this is a check point.

--- servicex/servicex.py
+++ servicex/servicex.py
@@ -3,15 +3,15 @@ import asyncio
 import functools
 import logging
 from pathlib import Path
-from typing import Any, Awaitable, Callable, Dict, Optional, Tuple
+from typing import Any, Awaitable, Callable, Dict, List, Optional, Tuple
 from typing import Iterator
 import urllib
 
 import aiohttp
 from make_it_sync import make_sync
 from minio import Minio
+from retry import retry
 
-from .servicexabc import ServiceXABC
 from .cache import cache
 from .data_conversions import _convert_root_to_awkward, _convert_root_to_pandas
 from .servicex_remote import (
@@ -20,6 +20,7 @@ from .servicex_remote import (
     _result_object_list,
     _submit_query,
 )
+from .servicexabc import ServiceXABC
 from .utils import (
     ServiceXException,
     ServiceXUnknownRequestID,
@@ -134,8 +135,28 @@ class ServiceX(ServiceXABC):
     get_data_awkward = make_sync(get_data_awkward_async)
     get_data_parquet = make_sync(get_data_parquet_async)
 
+    @retry(exceptions=ServiceXUnknownRequestID, tries=3)
     async def _data_return(self, selection_query: str,
                            converter: Callable[[Path], Awaitable[Any]]):
+        '''
+        Given a query, return the data, in a unique order, that hold
+        the data for the query.
+
+        For certian types of exceptions, the queries will be repeated. For example,
+        if `ServiceX` indicates that it was restarted in the middle of the query, then
+        the query will be re-submitted.
+
+        Arguments:
+
+            selection_query     `qastle` data that makes up the selection request.
+            converter           A `Callable` that will convert the data returned from
+                                `ServiceX` as a set of files.
+
+        Returns:
+
+            data                Data converted to the "proper" format, depending
+                                on the converter call.
+        '''
         # Get all the files
         as_files = (f async for f in self._get_files(selection_query, 'root-file'))  # type: ignore
 
@@ -150,7 +171,9 @@ class ServiceX(ServiceXABC):
 
         return ordered_data
 
+    @retry(exceptions=ServiceXUnknownRequestID)
     async def _file_return(self, selection_query: str, data_format: str):
+        # TODO: Isn't this just a special case of the get_data guy?
         '''
         Internal routine to refactor any query that will return a list of files (as opposed
         to in-memory data).
@@ -212,11 +235,6 @@ class ServiceX(ServiceXABC):
         Returns:
 
             None            Done when no more files are left.
-
-        Note:
-
-            - Raise the `_RetryException` if this is the first query and ServiceX does not
-              know about the exception.
         '''
         done = False
         last_processed = 0
@@ -237,11 +255,95 @@ class ServiceX(ServiceXABC):
         finally:
             downloader.shutdown()
 
+    async def _get_request_id(self, client: aiohttp.ClientSession, query: Dict[str, Any]):
+        '''
+        For this query, fetch the request id. If we have it cached, use that. Otherwise, query
+        ServiceX for a enw one (and cache it for later use).
+        '''
+        request_id = self._cache.lookup_query(query)
+        if request_id is None:
+            request_id = await _submit_query(client, self._endpoint, query)
+            self._cache.set_query(query, request_id)
+        return request_id
+
+    async def _get_cached_files(self, cached_files: List[Tuple[str, str]]):
+        '''
+        Return the list of files as an iterator that we have pulled from the cache
+        '''
+        self._notifier.update(processed=len(cached_files), remaining=0, failed=0)
+        loop = asyncio.get_event_loop()
+        for f, p in cached_files:
+            self._notifier.inc(downloaded=1)
+            path_future = loop.create_future()
+            path_future.set_result(Path(p))
+            yield f, path_future
+
+    async def _get_files_from_servicex(self, request_id: str,
+                                       client: aiohttp.ClientSession,
+                                       query: Dict[str, str]):
+        '''
+        Fetch query result files from `servicex`. Given the `request_id` we will download
+        files as they become available. We also coordinate caching.
+        '''
+        try:
+            minio = self._minio_client()
+            results_from_query = _result_object_list(minio, request_id)
+
+            # Problem - if this throws, then we want to get out of this, and we will be stuck
+            # waiting below too.
+            r_loop = asyncio.ensure_future(self._get_status_loop(client, request_id,
+                                                                 results_from_query))
+
+            try:
+                file_object_list = []
+                async for f in results_from_query.files():
+                    copy_to_path = self._file_name_func(request_id, f)
+
+                    async def do_wait(final_path):
+                        assert request_id is not None
+                        await _download_file(minio, request_id, f, final_path)
+                        self._notifier.inc(downloaded=1)
+                        self._notifier.broadcast()
+                        return final_path
+
+                    file_object_list.append((f, str(copy_to_path)))
+                    yield f, do_wait(copy_to_path)
+            finally:
+                await r_loop
+
+            # Now that data has been moved back here, lets make sure there were no failed
+            # files. If there were, then we need to mark this whole transform as
+            # having failed, and remove any trace of it in our caches so that if the user
+            # wants to re-try, then can do it as they see fit.
+            if self._notifier.failed > 0:
+                self._cache.remove_query(query)
+                raise ServiceXException(f'ServiceX failed to transform '
+                                        f'{self._notifier.failed}'
+                                        ' files - data incomplete.')
+
+            # If we got here, then the request finished! Cache the results! Woo!
+            self._cache.set_files(request_id, file_object_list)
+
+        except ServiceXUnknownRequestID:
+            # If servicex can't find this query, then we need to forget about it locally
+            # too.
+            self._cache.remove_query(query)
+
+            raise
+
     async def _get_files(self, selection_query: str, data_type: str) \
             -> Iterator[Tuple[str, Awaitable[Path]]]:
         '''
         Return a list of files from servicex as they have been downloaded to this machine. The
         return type is an awaitable that will yield the path to the file.
+
+        For certian types of `ServiceX` failures we will automatically attempt a few retries:
+
+            - When `ServiceX` forgets the query. This sometimes happens when a user submits a
+              query, and then disconnects from the network, `ServiceX` is restarted, and then the
+              user attempts to download the files from that "no-longer-existing" request.
+
+        Up to 3 re-tries are attempted automatically.
 
         Arguments:
 
@@ -254,66 +356,21 @@ class ServiceX(ServiceXABC):
                                         This is returned this way so a number of downloads can run
                                         simultaneously.
         '''
+        # TODO: Notifier is a per-query item, not a per dataset, needs to be created
+        #       for each query, not a single one.
         query = self._build_json_query(selection_query, data_type)
-        request_id = self._cache.lookup_query(query)
 
         async with aiohttp.ClientSession() as client:
-            if request_id is None:
-                request_id = await _submit_query(client, self._endpoint, query)
-                self._cache.set_query(query, request_id)
 
+            # Get a request id - which might be cached, but if not, submit it.
+            request_id = await self._get_request_id(client, query)
+
+            # Look up the cache, and then fetch an iterator going thorugh the results
+            # from either servicex or the cache, depending.
             cached_files = self._cache.lookup_files(request_id)
-            if cached_files is not None:
-                self._notifier.update(processed=len(cached_files), remaining=0, failed=0)
-                loop = asyncio.get_event_loop()
-                for f, p in cached_files:
-                    self._notifier.inc(downloaded=1)
-                    path_future = loop.create_future()
-                    path_future.set_result(Path(p))
-                    yield f, path_future
-            else:
-                minio = self._minio_client()
-                results_from_query = _result_object_list(minio, request_id)
+            fetched_files_seq = self._get_cached_files(cached_files) if cached_files is not None \
+                else self._get_files_from_servicex(request_id, client, query)
 
-                # Problem - if this throws, then we want to get out of this, and we will be stuck
-                # waiting below too.
-                r_loop = asyncio.ensure_future(self._get_status_loop(client, request_id,
-                                                                     results_from_query))
-
-                file_object_list = []
-                async for f in results_from_query.files():
-                    copy_to_path = self._file_name_func(request_id, f)
-
-                    async def do_wait(final_path):
-                        assert request_id is not None
-                        await _download_file(minio, request_id, f, final_path)
-                        self._notifier.inc(downloaded=1)
-                        self._notifier.broadcast()
-                        return final_path
-                    file_object_list.append((f, str(copy_to_path)))
-                    yield f, do_wait(copy_to_path)
-
-                # TODO: Pull this out to make sure this works correctly at a high level.
-                # This is torture code.
-                retry_me = False
-                try:
-                    await r_loop
-                except ServiceXUnknownRequestID:
-                    retry_me = True
-
-                if retry_me:
-                    self._cache.remove_query(query)
-                    async for r in self._get_files(selection_query, data_type):  # type: ignore
-                        yield r
-
-                # Now that data has been moved back here, lets make sure there were no failed
-                # files.
-                if self._notifier.failed > 0:
-                    # Take back the cache line
-                    self._cache.remove_query(query)
-                    raise ServiceXException(f'ServiceX failed to transform '
-                                            f'{self._notifier.failed}'
-                                            ' files - data incomplete.')
-
-                # If we got here, then the request finished.
-                self._cache.set_files(request_id, file_object_list)
+            # Reflect the files back up a level.
+            async for r in fetched_files_seq:
+                yield r
