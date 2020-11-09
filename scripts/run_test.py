87e85f25a04d527971c86ed0fbae9427819cc83e
Gordon Watts <gordon@gordonandpaula.net>
2020-11-08 21:35:10 -0500

    update script to do integrated testing
    This si more extensive - starting adding options to test other thing!

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -3,28 +3,53 @@
 # do that sort of testing.
 # An example endpoint (pass as arg to this script):
 #       http://localhost:5000
-import sys
+import argparse
+from typing import Optional
+import asyncio
+
 from servicex import ServiceXDataset
 from servicex.servicex_adaptor import ServiceXAdaptor
-from typing import Optional
 
 
-def run_query(endpoint: Optional[ServiceXAdaptor]) -> None:
+async def run_query(endpoint: Optional[ServiceXAdaptor], dest:str) -> None:
     ds = ServiceXDataset(
         "mc16_13TeV:mc16_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.deriv.DAOD_STDM3.e3601_e5984_s3126_r10201_r10210_p3975_tid20425969_00",  # NOQA
         backend_type='xaod',
         max_workers=100,
         servicex_adaptor=endpoint)
 
-    r = ds.get_data_rootfiles("(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')")  # NOQA
-    print(r)
+    request = "(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')"
+    if dest == 'rootfiles':
+        r = ds.get_data_rootfiles(request)  # NOQA
+        print(r)
+    elif dest == 'rootfiles-minio':
+        r = ds.get_data_rootfiles_minio_async(request)
+        async for f in r:
+            print(f)
 
 
 if __name__ == '__main__':
-    # import logging
-    # ch = logging.StreamHandler()
-    # ch.setLevel(logging.DEBUG)
-    # logging.getLogger('servicex').setLevel(logging.DEBUG)
-    # logging.getLogger('servicex').addHandler(ch)
-    servicex_adaptor = ServiceXAdaptor(sys.argv[1]) if len(sys.argv) >= 2 else None
-    run_query(servicex_adaptor)
+    # Setup the arguments we can deal with here.
+    parser = argparse.ArgumentParser(description='test servicex frontend.')
+    parser.add_argument('--log', dest='logging', action='store_const',
+                        const=True, default=False,
+                        help='Turn on the logging')
+    parser.add_argument('--output', dest='output',
+                        default=['rootfiles'], nargs=1,
+                        choices=['rootfiles', 'rootfiles-minio'],
+                        help='What output format should be returned')
+    args = parser.parse_args()
+
+    if args.logging:
+        import logging
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        logging.getLogger('servicex').setLevel(logging.DEBUG)
+        logging.getLogger('servicex').addHandler(ch)
+
+    # servicex_adaptor = ServiceXAdaptor(sys.argv[1]) if len(sys.argv) >= 2 else None
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(run_query(None, args.output[0]))
+    finally:
+        loop.close()
