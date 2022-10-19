b379bdbcb93df4d57b449695fe788f65cdf4160c
Matthew Feickert <matthew.feickert@cern.ch>
2022-10-19 18:16:45 -0500

    feat: Add nox support
    * Provide OS agnostic workflow runner with nox and workflows ("sessions")
      for linting, testing, reporting coverage, and building the package.
    * Add directories to exclude to .flake8

---
+++ noxfile.py
@@ -0,0 +1,78 @@
+import shutil
+from pathlib import Path
+
+import nox
+
+ALL_PYTHONS = ["3.7", "3.8", "3.9", "3.10", "3.11"]
+
+nox.options.sessions = ["lint", "tests"]
+
+
+DIR = Path(__file__).parent.resolve()
+
+
+@nox.session(reuse_venv=True)
+def lint(session):
+    """
+    Lint with flake8.
+    """
+    session.install("--upgrade", "flake8")
+    session.run("flake8", "--exclude=tests/*", "--ignore=E501,W503", *session.posargs)
+
+
+@nox.session(python=ALL_PYTHONS, reuse_venv=True)
+def tests(session):
+    """
+    Run the unit tests under coverage.
+    Specify a particular Python version with --python option.
+
+    Example:
+
+        $ nox --session tests --python 3.11
+    """
+    session.install("--upgrade", "--editable", ".[test]")
+    session.install("--upgrade", "pytest")
+    session.run(
+        "pytest",
+        "--ignore=setup.py",
+        "--cov=servicex",
+        "--cov-report=term-missing",
+        "--cov-config=.coveragerc",
+        "--cov-report=xml",
+        *session.posargs,
+    )
+
+
+@nox.session(reuse_venv=True)
+def coverage(session):
+    """
+    Generate coverage report
+    """
+    session.install("--upgrade", "pip")
+    session.install("--upgrade", "coverage[toml]")
+
+    session.run("coverage", "report")
+    session.run("coverage", "xml")
+    htmlcov_path = DIR / "htmlcov"
+    if htmlcov_path.exists():
+        session.log(f"rm -r {htmlcov_path}")
+        shutil.rmtree(htmlcov_path)
+    session.run("coverage", "html")
+
+
+@nox.session
+def build(session):
+    """
+    Build a sdist and wheel.
+    """
+
+    # cleanup previous build and dist dirs
+    build_path = DIR.joinpath("build")
+    if build_path.exists():
+        shutil.rmtree(build_path)
+    dist_path = DIR.joinpath("dist")
+    if dist_path.exists():
+        shutil.rmtree(dist_path)
+
+    session.install("build")
+    session.run("python", "-m", "build")
