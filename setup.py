ec1e541a3830e99b78e86451304e50dbf310decb
talvandaalen <talvandaalen@hotmail.com>
2023-04-11 16:39:28 +0200

    Setting upper bound for awkward version from 2 to 3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "idna==2.10",  # Required to thread version needle with requests library
         "pandas~=1.0",
         "uproot>=4.0.1, <5",
-        "awkward>=1.0.1, <2",
+        "awkward>=1.0.1, <3",
         "backoff>=2.0",
         "aiohttp~=3.6",
         "minio~=5.0",
