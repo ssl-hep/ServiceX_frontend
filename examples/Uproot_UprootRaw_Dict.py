1f100158d8ab7e968fe51aecb6b78931b50d54fd
Peter Onyisi <ponyisi@utexas.edu>
2024-07-05 18:23:25 +0000

    Signal transform error

--- examples/Uproot_UprootRaw_Dict.py
+++ examples/Uproot_UprootRaw_Dict.py
@@ -1,4 +1,5 @@
 from servicex import UprootRaw, deliver
+import sys
 
 
 def uproot_uproot_raw_dict():
@@ -16,4 +17,5 @@ def uproot_uproot_raw_dict():
 
 
 if __name__ == "__main__":
-    uproot_uproot_raw_dict()
+    files = uproot_uproot_raw_dict()
+    sys.exit(0 if len(files['Uproot_PythonFunction_Dict']) == 27 else 1)
