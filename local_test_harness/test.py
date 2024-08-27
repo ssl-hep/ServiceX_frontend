1fd370d95fbf20f012a764b724eddefb093093a9
Ketan <ketanbmahajan@gmail.com>
2024-08-27 14:57:25 -0500

    updated the compose file and added an environment file

--- local_test_harness/test.py
+++ local_test_harness/test.py
@@ -9,21 +9,35 @@ Input: Query, Root file and output format
 import requests
 import subprocess
 import os
-import json
 from requests_toolbelt.multipart import decoder
 from io import BytesIO
 from zipfile import ZipFile
 import time
+import yaml
+import argparse
 
 
-def start_codegen_container():
-    subprocess.run(["docker", "run", "-d", "-p", "8000:5000", "--name", "code_gen",
-                    "sslhep/servicex_code_gen_raw_uproot:develop"])
+class ConfigReader:
+    
+    def __init__(self, file):
+        self._file = file
+        self._config = None
 
+    def read_config_file(self):
+        with open(self._file) as yaml_file:
+            self._config = yaml.safe_load(yaml_file.read())
 
-def send_request(query):
-    post_url = "http://localhost:8000"
-    query = json.dumps(query)
+    def get_config(self):
+        return self._config
+
+def docker_compose_up():
+    subprocess.run(["docker", "compose", "up", "-d", "--remove-orphans"])
+
+def docker_compose_down():
+    subprocess.run(["docker", "compose", "down"])
+
+def send_request(query, port):
+    post_url = f"http://localhost:{port}"
     postObj = {"code": query}
     result = requests.post(post_url + "/servicex/generated-code", json=postObj)
     return result
@@ -39,7 +53,7 @@ def generate_zipfile(result, output_folder):
     zipfile.extractall(output_folder)
 
 
-def run_docker_compose_for_science():
+def run_docker_compose_for_science(image, filepath):
     subprocess.run(["docker", "compose", "up", "-d", "--remove-orphans"])
 
 
@@ -49,24 +63,39 @@ def send_root_file_to_science(root_file, output_file, output_format):
                     root_file, output_file, output_format])
 
 
+def run_x509_proxy(proxy_image = "sslhep/x509-secrets:develop"):
+    my_env = os.environ.copy()
+    subprocess.run(["docker" ,"run", "-it", "--mount", f"type=bind,source={my_env['HOME']}/.globus,readonly,target=/globus",
+                    "-v", "/tmp:/tmp", "--rm", proxy_image,
+                    "voms-proxy-init", "-voms", "atlas", "-cert",
+                    "/globus/usercert.pem", "-key", "/globus/userkey.pem" ,"-out", "/tmp/x509up"
+                    ]
+                    )
+
+
 if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser(description='Run the servicex codegen and science container')
+        parser.add_argument('--proxy_image', help='Run the x509 proxy for image')
+        args = parser.parse_args()
+        if args.proxy_image:
+            run_x509_proxy(proxy_image = args.proxy_image)
+        
+        a = ConfigReader("config.yml")
+        a.read_config_file()
+        docker_compose_up()
+        time.sleep(10)
 
-    # start codegen
-    start_codegen_container()
-    time.sleep(10)
+        result = send_request(
+                            query=a.get_config()['codegen']['query'] , #query,#,
+                            port=a.get_config()['codegen']['port'])
+        generate_zipfile(result, output_folder="temp1")
 
-    # send request and extract the zip file to folder
-    query = [{'treename': {'nominal': 'modified'}, 'filter_name': ['lbn']}]
-    result = send_request(query=query)
-    generate_zipfile(result, output_folder="temp1")
 
-    # run the docker compose for the science container
-    run_docker_compose_for_science()
-    time.sleep(10)
-
-    # send the root file to the science container
-    send_root_file_to_science(
-        root_file="root://xrootd.aglt2.org:1094//pnfs/aglt2.org/atlasdatadisk/rucio/mc20_13TeV/d4/32/DAOD_PHYS.37620644._000804.pool.root.1",  # noqa: E501
-        output_file="/generated/out.parquet",
-        output_format="root-file"
-    )
+        send_root_file_to_science(
+            root_file= a.get_config()['science']['rootfile'],
+            output_file=a.get_config()['science']['outputfile'],
+            output_format=a.get_config()['science']['outputformat']
+        )
+    finally:
+        docker_compose_down()
