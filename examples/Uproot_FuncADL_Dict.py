48d590fb591788dd7a1a2aa6239c2147aa86cec0
Ilija Vukotic <ivukotic@uchicago.edu>
2024-08-29 09:48:41 -0500

    Update Uproot_FuncADL_Dict.py
    this works

--- examples/Uproot_FuncADL_Dict.py
+++ examples/Uproot_FuncADL_Dict.py
@@ -6,16 +6,13 @@ def uproot_funcadl_dict():
         Select(lambda e: {'el_pt_NOSYS': e['el_pt_NOSYS']})  # type: ignore
 
     spec = {
-        'General': {
-            'ServiceX': "servicex-uc-af"
-        },
         'Sample': [{
             'Name': "Uproot_FuncADL_Dict",
             'RucioDID': "user.mtost:user.mtost.singletop.p6026.Jun13",
             'Query': query
         }]
     }
-    return deliver(spec)
+    return deliver(spec, servicex_name="servicex-uc-af")
 
 
 if __name__ == "__main__":
