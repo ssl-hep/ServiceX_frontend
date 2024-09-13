0ad5a2e092750b3030d3c2028907f51c925c7196
KyungEon Choi <kyungeon@gmail.com>
2024-09-13 11:23:11 -0500

    flake8

--- examples/FuncADL_Uproot_Typed.py
+++ examples/FuncADL_Uproot_Typed.py
@@ -7,10 +7,10 @@ spec = ServiceXSpec(
             Name="FuncADL_Uproot_Typed",
             Dataset=dataset.FileList(
                 [
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",  # noqa: E501
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",  # noqa: E501
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",  # noqa: E501
-            ]
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",  # noqa: E501
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",  # noqa: E501
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",  # noqa: E501
+                ]
             ),
             Query=query.FuncADL_Uproot()
                        .FromTree('CollectionTree')
