f318c59206cd2d154a20b4b1da94084cea0e1762
KyungEon Choi <kyungeon@gmail.com>
2024-09-13 10:20:27 -0500

    Add and update examples

---
+++ examples/UprootRaw_Typed.py
@@ -0,0 +1,27 @@
+from servicex import Sample, ServiceXSpec, query, dataset, deliver
+
+
+spec = ServiceXSpec(
+    Sample=[
+        Sample(
+            Name="UprootRaw_Typed",
+            Dataset=dataset.FileList(
+                [
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",
+                    "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",
+                ]
+            ),
+            Query=query.UprootRaw(
+                [
+                    {
+                        "treename": "CollectionTree",
+                        "filter_name": "AnalysisElectronsAuxDyn.pt",
+                    }
+                ]
+            )
+        )
+    ]
+)
+
+print(f"Files: {deliver(spec)}")
