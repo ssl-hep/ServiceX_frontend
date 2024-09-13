225235ca2758cad020fe6719357b2f34767ff3da
KyungEon Choi <kyungeon@gmail.com>
2024-09-13 11:15:14 -0500

    Ignore flake for xrootd file names

--- examples/FuncADL_Uproot_Dict.py
+++ examples/FuncADL_Uproot_Dict.py
@@ -6,9 +6,9 @@ spec = {
         'Name': "FuncADL_Uproot_Dict",
         'Dataset': dataset.FileList(
             [
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",
-                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",
+                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",  # noqa: E501
+                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",  # noqa: E501
+                "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",  # noqa: E501
             ]
         ),
         'Query': query.FuncADL_Uproot()
