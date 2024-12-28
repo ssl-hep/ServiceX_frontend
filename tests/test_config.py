7e57310f0df641bcd59e457c113a02a173f0572c
Gorodn Watts <gwatts@uw.edu>
2024-12-27 17:33:19 -0700

    Add tests for endpoint dict

--- tests/test_config.py
+++ tests/test_config.py
@@ -52,7 +52,22 @@ def test_config_read(tempdir):
         Configuration.read(config_path="invalid.yaml")
 
 
-@patch("servicex.configuration.tempfile.gettempdir", return_value="./mytemp")
+@patch('servicex.configuration.tempfile.gettempdir', return_value="./mytemp")
+def test_config_endpoint_dict(tempdir):
+    os.environ['UserName'] = "p_higgs"
+    c = Configuration.read(config_path="tests/example_config.yaml")
+    endpoints = c.endpoint_dict()
+    assert len(endpoints) == 3
+    assert "servicex-uc-af" in endpoints
+
+    # Make sure we get back what we expect
+    ep = endpoints["servicex-uc-af"]
+    assert ep.endpoint == "https://servicex.af.uchicago.edu"
+    assert ep.name == "servicex-uc-af"
+    assert ep.token == "notreallyatoken"
+
+
+@patch('servicex.configuration.tempfile.gettempdir', return_value="./mytemp")
 def test_default_cache_path(tempdir):
 
     # Windows style user name
