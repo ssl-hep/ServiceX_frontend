936c6bab9ef644b3bbbf29b8fd40c2f0e8d329d8
KyungEon Choi <kyungeon@gmail.com>
2025-03-10 13:11:27 -0500

    Update example

--- examples/TopCP_Typed.py
+++ examples/TopCP_Typed.py
@@ -1,20 +1,23 @@
-from servicex import query, dataset, deliver
+from servicex import Sample, ServiceXSpec, query, dataset, deliver
 
 
-spec = {
-    "Sample": [
-        {
-            "Name": "TopCP_Dict",
-            "Dataset": dataset.FileList(
+spec = ServiceXSpec(
+    Sample=[
+        Sample(
+            Name="TopCP_Typed",
+            Dataset=dataset.FileList(
                 [
                     "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000001.pool.root.1",  # noqa: E501
                     "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000002.pool.root.1",  # noqa: E501
                     "root://eospublic.cern.ch//eos/opendata/atlas/rucio/data16_13TeV/DAOD_PHYSLITE.37019878._000003.pool.root.1",  # noqa: E501
                 ]
             ),
-            "Query": query.TopCP(reco="reco.yaml", max_events=1000),
-        }
+            Query=query.TopCP(
+                reco="reco.yaml",
+                max_events=1000
+            ),
+        )
     ]
-}
+)
 
 print(f"Files: {deliver(spec)}")
