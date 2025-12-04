1fe7cae407a0afc7c68304499201a35bb0c8248e
Matt Shirley <mattutshirley@gmail.com>
2025-12-04 12:52:59 -0600

    update key

--- tests/test_topcp_dataset.py
+++ tests/test_topcp_dataset.py
@@ -125,7 +125,7 @@ CommonServices:
     )
 
     docker_image = "my-custom-image:latest"
-    topcp_query = TopCPQuery(reco=reco_file, docker_image=docker_image)
+    topcp_query = TopCPQuery(reco=reco_file, image=docker_image)
     query_string = topcp_query.generate_selection_string()
     query = json.loads(query_string)
 
