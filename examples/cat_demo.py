f3012d6bf670ba7ee9324da9acd2266e8f770d9e
Ben Galewsky <ben@peartreestudio.net>
2026-02-12 15:29:47 -0600

    Add a version attribute to the cache. Allow users to specify a version  with a deliver invocation

--- examples/cat_demo.py
+++ examples/cat_demo.py
@@ -5,13 +5,26 @@ from servicex.catalog import Catalog
 cat = Catalog(Path("/Users/bengal1/dev/IRIS-HEP/ServiceX_Client/cache-dir"))
 print(cat.samples)
 
-print(cat.db.all())
 uproot_cat = cat["UprootRaw_YAML"]
-print(uproot_cat.get_runs())
+print(uproot_cat.get_versions())
+
+
+from rich.console import Console
+from rich.table import Table
+
+table = Table(title="Uproot Catalog Runs")
+table.add_column("SHA", style="cyan")
+table.add_column("RequestID", style="yellow")
+table.add_column("Submit Time", style="magenta")
+table.add_column("Version", style="green")
 
 for sha in uproot_cat.get_runs():
-    print(sha, uproot_cat.get_run(sha).submit_time)
+    run = uproot_cat.get_run(sha)
+    table.add_row(sha, run.request_id, str(run.submit_time), run.version)
+
+console = Console()
+console.print(table)
 
 
 print(uproot_cat.latest.submit_time)
-print(uproot_cat.latest.file_list)
+print(uproot_cat.get_version("1.0").file_list)
--- servicex/catalog.py
+++ servicex/catalog.py
@@ -10,15 +10,11 @@ from servicex.models import TransformedResults
 class Catalog:
     def __init__(self, path: Path):
         self.path = path
-        self.db = TinyDB(
-                os.path.join(self.path, ".servicex", "db.json")
-            )
-
-
+        self.db = TinyDB(os.path.join(self.path, ".servicex", "db.json"))
 
     @property
     def samples(self) -> list[str]:
-        distinct_titles = {doc['title'] for doc in self.db.all()}
+        distinct_titles = {doc["title"] for doc in self.db.all()}
         return list(distinct_titles)
 
     def get_sample(self, title: str) -> list[TransformedResults]:
@@ -29,15 +25,22 @@ class Catalog:
         """
         transforms = Query()
 
-        sample_recs = self.db.search((transforms.title == title) & (transforms.status == 'COMPLETE'))
-        sample_recs.sort(key=lambda x: datetime.fromisoformat(x['submit_time'].replace('Z', '+00:00')))
+        sample_recs = self.db.search(
+            (transforms.title == title) & (transforms.status == "COMPLETE")
+        )
+        sample_recs.sort(
+            key=lambda x: datetime.fromisoformat(
+                x["submit_time"].replace("Z", "+00:00")
+            )
+        )
 
-        return [ TransformedResults(**rec) for rec in sample_recs]
+        return [TransformedResults(**rec) for rec in sample_recs]
 
     def __getitem__(self, item):
         """
         Allow the catalog to be indexed by sample title."""
         return Sample(self.get_sample(item))
+
 
 class Sample:
     def __init__(self, results: list[TransformedResults]):
@@ -46,8 +49,14 @@ class Sample:
     def get_runs(self) -> list[str]:
         return [run.short_hash for run in self.results]
 
+    def get_versions(self) -> list[str]:
+        return [run.version for run in self.results if run.version is not None]
+
     def get_run(self, sha: str) -> TransformedResults:
         return next(filter(lambda x: x.short_hash == sha, self.results))
+
+    def get_version(self, version: str) -> TransformedResults:
+        return next(filter(lambda x: x.version == version, self.results))
 
     @property
     def latest(self) -> TransformedResults:
